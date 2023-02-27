6935801dd8610497c8b83a16a4224ab2392f178c
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2023-02-28 01:20:13 +0200

    Initial Firebase Commit - reading from database

--- app/src/main/java/com/example/orientgardenneighbourhoodwatch/MainActivity.java
+++ app/src/main/java/com/example/orientgardenneighbourhoodwatch/MainActivity.java
@@ -1,20 +1,46 @@
 package com.example.orientgardenneighbourhoodwatch;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ArrayList<Incident> incidentsList = new ArrayList<>();
+    private IncidentAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        adapter = new IncidentAdapter(this, incidentsList);
+        ListView incidentListView = findViewById(R.id.main_ListView);
+        incidentListView.setAdapter(adapter);
+
+
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference("ogwatchDB");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
     }
 }
