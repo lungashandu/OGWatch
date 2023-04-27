aaa3b400e98bc1c2278da46b6e73c87f14c5d418
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2023-04-27 18:22:35 +0200

    Sign in with google

---
+++ app/src/main/java/com/example/orientgardenneighbourhoodwatch/SubmitButton.java
@@ -0,0 +1,27 @@
+package com.example.orientgardenneighbourhoodwatch;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class SubmitButton extends androidx.appcompat.widget.AppCompatButton {
+    public SubmitButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public SubmitButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SubmitButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    private void init() {
+        setElevation((float) 1.0);
+    }
+}
