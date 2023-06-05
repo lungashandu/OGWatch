747e60e51c7c273cc43e734f86f52f4d5754ad50
Lunga Mbatha <51107227+lungashandu@users.noreply.github.com>
2023-06-05 14:01:08 +0200

    Inital commit

--- app/src/main/java/com/example/orientgardenneighbourhoodwatch/ViewIncident.java
+++ app/src/main/java/com/example/orientgardenneighbourhoodwatch/ViewIncident.java
@@ -1,6 +1,7 @@
 package com.example.orientgardenneighbourhoodwatch;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,6 +9,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +29,8 @@ import java.util.concurrent.Executors;
 public class ViewIncident extends AppCompatActivity {
     private Incident incident;
     private String imageUrl;
+    private RequestQueue requestQueue;
+    private ImageView incidentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +47,9 @@ public class ViewIncident extends AppCompatActivity {
         TextView stolenItem = findViewById(R.id.vp_stolenItemTextView);
         TextView houseNumber = findViewById(R.id.vp_houseNumberTextView);
         TextView description = findViewById(R.id.vp_descriptionTextView);
-        ImageView incidentImage = findViewById(R.id.vp_stolenItemImageView);
+        incidentImage = findViewById(R.id.vp_stolenItemImageView);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(() -> reference.addValueEventListener(new ValueEventListener() {
@@ -63,6 +73,21 @@ public class ViewIncident extends AppCompatActivity {
         incidentImage.setOnClickListener(view -> {
             showImageDialog();
         });
+    }
+
+    private void loadImageFromUrl(String imageUrl) {
+        ImageRequest imageRequest = new ImageRequest(imageUrl, new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                incidentImage.setImageBitmap(response);
+            }
+        }, 0, 0, null, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                incidentImage.setImageResource(R.drawable.ic_baseline_no_photography_24);
+            }
+        });
+        requestQueue.add(imageRequest);
     }
 
     private void showImageDialog() {
